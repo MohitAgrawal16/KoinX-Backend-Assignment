3c41d0a2e3e431b77b5b68fc3ae1f99d98539671
MohitAgrawal16 <mohitagrawal1684@gmail.com>
2025-01-10 19:02:21 +0530

    created model of MarketData

---
+++ src/models/MarketData.model.js
@@ -0,0 +1,26 @@
+import mongoose, {Schema} from "mongoose";
+
+const MarketDataSchema = new Schema({
+
+    coin:{
+        type: String,
+        required: true
+    },
+    price:{
+        type: Number,
+        required: true
+    },
+    marketCap:{
+        type: Number,
+        required: true
+    },
+    change24h:{
+        type: Number,
+        required: true
+    },
+
+},{timestamps: true});  
+
+const MarketData = mongoose.model('MarketData', MarketDataSchema);
+
+export {MarketData};
